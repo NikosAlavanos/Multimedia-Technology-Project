c4cac28141094bf5fd5bafddc7bee1421f3c52b0
NikosAlavanos <106879186+NikosAlavanos@users.noreply.github.com>
2024-10-30 21:55:02 +0200

    Inventory System, without stats

--- Assets/Scripts/Items&&Inventory/ItemData_Equipment.cs
+++ Assets/Scripts/Items&&Inventory/ItemData_Equipment.cs
@@ -13,4 +13,34 @@ public enum EquipmentType
 public class ItemData_Equipment : ItemData
 {
     public EquipmentType equipmentType;
+
+    [Header("Core stats")]
+    public int strength;
+    public int dexterity;
+    public int vitality;
+
+    [Header("Offensive stats")]
+    public int damage;
+    public int critChance;
+    public int critDamage;
+
+    [Header("Defensive stats")]
+    public int health;
+    public int armor;
+    public int evasion;
+    public int magicResistance;
+
+    [Header("Magic stats")]
+    public int fireDamage;
+    public int iceDamage;
+    public int lightingDamage;
+    public void AddModifiers()
+    {
+
+    }
+
+    public void RemoveModifiers()
+    {
+
+    }
 }
--- Assets/Scripts/UI/UI_ItemSlot.cs
+++ Assets/Scripts/UI/UI_ItemSlot.cs
@@ -1,8 +1,9 @@
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class UI_ItemSlot : MonoBehaviour
+public class UI_ItemSlot : MonoBehaviour , IPointerDownHandler
 {
     [SerializeField] private Image itemImage;
     [SerializeField] private TextMeshProUGUI itemText;
@@ -30,5 +31,20 @@ public class UI_ItemSlot : MonoBehaviour
             }
 
         }
+    }
+
+    public void CleanUpSlot()
+    {
+        item = null;
+
+        itemImage.sprite = null;
+        itemImage.color = Color.clear;
+        itemText.text = "";
+    }
+
+    public void OnPointerDown(PointerEventData eventData) //its called whenever you use your pointer down on this object that has this Interface
+    {
+        if (item.data.itemType == ItemType.Equipment)
+            Inventory.instance.EquipItem(item.data);
     }
 }
