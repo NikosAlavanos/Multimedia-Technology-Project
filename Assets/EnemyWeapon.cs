cd9d69c835475b46a5565b1b14204e7eb669a7d7
NikosAlavanos <106879186+NikosAlavanos@users.noreply.github.com>
2025-01-06 16:15:15 +0200

    changes scripts

--- Assets/EnemyWeapon.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemyWeapon : MonoBehaviour
-{
-    public int attackDamage = 20;
-    public int enragedAttackDamage = 40;
-}
